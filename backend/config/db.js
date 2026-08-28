38fea3088adb0d2100866b1d67f58cab2353472b
Vinaya kumar <vinay@gritty-tech.com>
2026-08-28 11:29:07 +0530

    .env changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URI)
 
         console.log("Database connection successful")
     } catch (error) {
